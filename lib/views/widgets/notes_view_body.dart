d9f741a5d712e55c859a37cedcc26d592b51b1da
Abdulnaseer <abdulnasir.alkhatib@gmail.com>
2023-03-12 12:16:35 -0400

    ctreate Notes list view

--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/views/widgets/custom_app_bar.dart';
-import 'package:notes_app/views/widgets/note_item.dart';
+import 'package:notes_app/views/widgets/notes_list_view.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
@@ -14,7 +14,7 @@ class NotesViewBody extends StatelessWidget {
           SizedBox(height: MediaQuery.of(context).size.height * 0.05),
           const CustomAppBar(),
           const SizedBox(height: 20),
-          const NoteItem(),
+          Expanded(child: NotesListView()),
         ],
       ),
     );
